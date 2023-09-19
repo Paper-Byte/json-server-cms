b246c094e9e4120bb9198b2210daab57565868f2
Noah <noahhughesph@gmail.com>
2023-09-19 15:11:08 -0400

    changes port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const jsonServer = require('json-server');
+const cors = require('cors');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const router = jsonServer.router(
+  path.join(__dirname, 'db', 'db.json')
+);
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
@@ -10,7 +12,7 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
